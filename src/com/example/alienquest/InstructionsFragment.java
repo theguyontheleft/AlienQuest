50157f7d7d7e6d1d8aec45d218d1c6c877fa5608
Garrett Moran <gemoran4@vt.edu>
2014-03-30 17:00:29 -0400

    added xmls, inits for classes

--- src/com/example/alienquest/InstructionsFragment.java
+++ src/com/example/alienquest/InstructionsFragment.java
@@ -1,6 +1,27 @@
 package com.example.alienquest;
 
-public class InstructionsFragment
-{
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+public class InstructionsFragment
+    extends Fragment
+{
+    // this is the first callback method that is invoked.
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+    }
+
+ // this is where the GUI is initialized
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.instructions_fragment,
+                container, false);
+        return view;
+    }
 }

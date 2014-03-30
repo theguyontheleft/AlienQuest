ffe2ccd0d85ac63d22d2282d77364f8855fadd3f
James Dagres <jdagres@vt.edu>
2014-03-30 18:22:13 -0400

    Updated settings and the general project xmls.

--- src/com/example/alienquest/MainActivity.java
+++ src/com/example/alienquest/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.alienquest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.mapping.GPSLocator;
+import com.example.settings.SettingsPreferenceActivity;
 
 /**
  * @author Jimmy Dagres
@@ -24,7 +28,16 @@ public class MainActivity
      * Used for the settings
      */
     public static boolean inputCorrect;
-    
+
+    /**
+     * determines if the app is in its initial setup
+     */
+    private static boolean initialSetup;
+    /**
+     * settings menu Intent
+     */
+    private Intent settingsIntention;
+
     // GPS location values
     protected double longitude_;
     protected double latitude_;
@@ -37,14 +50,47 @@ public class MainActivity
         setContentView( R.layout.activity_main );
 
         getGPSLocation();
+
+        SettingsPreferenceActivity settings_ =
+                new SettingsPreferenceActivity();
+        settingsIntention =
+                new Intent( MainActivity.this,
+                        SettingsPreferenceActivity.class );
+    }
+
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item )
+    {
+        switch ( item.getItemId() )
+        {
+        case R.id.action_settings:
+            // MainActivity.this.startActivity( settingsIntention ); // Causes
+            // it to crash
+
+            break;
+        }
+
+        return true;
     }
 
     @Override
     public boolean onCreateOptionsMenu( Menu menu )
     {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate( R.menu.main, menu );
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate( R.menu.settings, menu );
         return true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see android.app.Activity#onMenuItemSelected(int, android.view.MenuItem)
+     */
+    @Override
+    public boolean onMenuItemSelected( int featureId, MenuItem item )
+    {
+        // TODO Auto-generated method stub
+        return super.onMenuItemSelected( featureId, item );
     }
 
     /**

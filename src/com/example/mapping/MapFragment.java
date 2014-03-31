f99e89b06293d9c0833dddb286560930580e298b
James Dagres <jdagres@vt.edu>
2014-03-31 19:10:11 -0400

    Added customer marker for map, started implemented google maps.

--- src/com/example/mapping/MapFragment.java
+++ src/com/example/mapping/MapFragment.java
@@ -1,13 +1,12 @@
 package com.example.mapping;
 
-import com.example.alienquest.R;
-import com.example.alienquest.R.layout;
-
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.alienquest.R;
 
 /**
  * @author Jimmy Dagres
@@ -23,11 +22,22 @@ public class MapFragment
 {
     private GPSLocator gps_ = null;
 
+   // private GoogleMap mMap;
+    
+// // Uses a custom icon.
+//    mSydney = mMap.addMarker(new MarkerOptions()
+//        .position(SYDNEY)
+//        .title("Sydney")
+//        .snippet("Population: 4,627,300")
+//        .icon(BitmapDescriptorFactory.fromResource(R.drawable.arrow)));
+    
     // this is the first callback method that is invoked.
     @Override
     public void onCreate(Bundle state) {
         super.onCreate(state);
     }
+    
+    
 
  // this is where the GUI is initialized
     @Override

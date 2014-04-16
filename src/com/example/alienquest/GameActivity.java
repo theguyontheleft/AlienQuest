84f84b4a5d5e6743baa425b5a297b63d17786bf3
Tiffany <tiffany.liana@gmail.com>
2014-04-16 18:04:53 -0400

    Updated CampaignSetUp by initializing number of random aliens

--- src/com/example/alienquest/GameActivity.java
+++ src/com/example/alienquest/GameActivity.java
@@ -21,6 +21,7 @@ import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.example.mapping.CampaignSetUp;
 import com.example.mapping.GPSLocator;
 import com.example.mapping.MapFragmentClass;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -42,313 +43,275 @@ import com.google.android.gms.maps.model.MarkerOptions;
  * 
  *          This activity will display the game mode
  */
-@SuppressLint( "NewApi" )
-public class GameActivity extends Activity implements SurfaceTextureListener
-{
-    public final static String FRAG1_TAG = "FRAG1";
-    public final static String FRAG2_TAG = "FRAG2";
+@SuppressLint("NewApi")
+public class GameActivity extends Activity implements SurfaceTextureListener {
+	public final static String FRAG1_TAG = "FRAG1";
+	public final static String FRAG2_TAG = "FRAG2";
 
-    private String userName_ = "";
+	private String userName_ = "";
 
-    private Camera mCamera;
-    private TextureView mTextureView;
+	private Camera mCamera;
+	private TextureView mTextureView;
 
-    private RelativeLayout mRelativeLayout;
+	private RelativeLayout mRelativeLayout;
 
-    private MapFragmentClass mapFragement_;
-    private ListView objectives;
-    private ArrayAdapter<String> itemAdapter;
-    private ArrayList<String> itemList;
-    private FrameLayout objectivesFrame;
+	private MapFragmentClass mapFragement_;
+	private ListView objectives;
+	private ArrayAdapter<String> itemAdapter;
+	private ArrayList<String> itemList;
+	private FrameLayout objectivesFrame;
 
-    /**
-     * The google map
-     */
-    protected static GoogleMap mMap_;
-    protected static MapFragment mMapFrag_;
+	/**
+	 * The google map
+	 */
+	protected static GoogleMap mMap_;
+	protected static MapFragment mMapFrag_;
 
-    protected GPSLocator gps_ = null;
+	protected GPSLocator gps_ = null;
 
-    // Store the width and height in pixels, this will be usefull in
-    // calculations throughout the application
-    private int screenWidthPixels_;
-    private int screenHeightPixels_;
+	// Store the width and height in pixels, this will be usefull in
+	// calculations throughout the application
+	private int screenWidthPixels_;
+	private int screenHeightPixels_;
 
-    // Time in ms that the game started
-    private static long questStartTime_;
+	// Time in ms that the game started
+	private static long questStartTime_;
 
-    /**
-     * When the game first starts the user is prompted of the map where the
-     * aliens are landing
-     */
-    private boolean userPrompted_ = false;
+	/**
+	 * When the game first starts the user is prompted of the map where the
+	 * aliens are landing
+	 */
+	private boolean userPrompted_ = false;
 
-    // GPS location values
-    protected double longitude_;
-    protected double latitude_;
+	// GPS location values
+	protected double longitude_;
+	protected double latitude_;
+	private CampaignSetUp setUp;
 
-    /**
-     * instance of the settings task
-     */
-    private static SharedPreferences preference_;
+	/**
+	 * instance of the settings task
+	 */
+	private static SharedPreferences preference_;
 
-    @Override
-    protected void onCreate( Bundle savedInstanceState )
-    {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_game );
-        objectives = (ListView) findViewById( R.id.objectivesFrame )
-                .findViewById( R.id.listView1 );
-        itemList = new ArrayList<String>();
-        itemList.clear();
-        itemAdapter =
-                new ArrayAdapter<String>( this, R.id.listView1, itemList );
-        objectives.setAdapter( itemAdapter );
-        objectives.bringToFront();
-        // Load the preference values
-        // Set up the preference
-        preference_ =
-                getSharedPreferences( getString( R.string.pref_title_file ),
-                        Context.MODE_PRIVATE );
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game);
+		objectives = (ListView) findViewById(R.id.objectivesFrame)
+				.findViewById(R.id.listView1);
+		itemList = new ArrayList<String>();
+		itemList.clear();
+		itemAdapter = new ArrayAdapter<String>(this, R.id.listView1, itemList);
+		objectives.setAdapter(itemAdapter);
+		objectives.bringToFront();
+		// Load the preference values
+		// Set up the preference
+		preference_ = getSharedPreferences(getString(R.string.pref_title_file),
+				Context.MODE_PRIVATE);
 
-        userName_ =
-                preference_.getString( getString( R.string.pref_title_name ),
-                        getString( R.string.pref_title_name ) );
-        if ( 1 <= userName_.length() )
-        {
-            Toast.makeText(
-                    getApplicationContext(),
-                    "Welcome "
-                            + userName_
-                            + " to the quest. The human race is counting on you"
-                            + " to fend off the alien spaceships attempting to land on earth.",
-                    Toast.LENGTH_LONG * 2 ).show();
-        }
+		userName_ = preference_.getString(getString(R.string.pref_title_name),
+				getString(R.string.pref_title_name));
+		if (1 <= userName_.length()) {
+			Toast.makeText(
+					getApplicationContext(),
+					"Welcome "
+							+ userName_
+							+ " to the quest. The human race is counting on you"
+							+ " to fend off the alien spaceships attempting to land on earth.",
+					Toast.LENGTH_LONG * 2).show();
+		}
 
-        getGPSLocation();
+		getGPSLocation();
 
-        // Start off by displaying a google map and inserting the aliens
+		// Start off by displaying a google map and inserting the aliens
 
-        // Store the screen width and height
-        DisplayMetrics metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics( metrics );
-        screenWidthPixels_ = metrics.widthPixels;
-        screenHeightPixels_ = metrics.heightPixels;
+		// Store the screen width and height
+		DisplayMetrics metrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(metrics);
+		screenWidthPixels_ = metrics.widthPixels;
+		screenHeightPixels_ = metrics.heightPixels;
 
-        // displayCamera();
-        displayMapFragment();
-        // displayObjectives();
+		// displayCamera();
+		displayMapFragment();
+		// displayObjectives();
 
-        // Record the start time of the game
-        questStartTime_ = System.currentTimeMillis();
-    }
+		// Record the start time of the game
+		questStartTime_ = System.currentTimeMillis();
+	}
 
-    /**
-     * Adds the map fragment to the view
-     */
-    private void displayMapFragment()
-    {
-        // Add the map fragment
-        mMapFrag_ = new MapFragment();
+	/**
+	 * Adds the map fragment to the view
+	 */
+	private void displayMapFragment() {
+		// Add the map fragment
+		mMapFrag_ = new MapFragment();
 
-        FragmentTransaction fragMan =
-                getFragmentManager().beginTransaction();
-        fragMan.add( R.id.mapFrame, mMapFrag_, FRAG2_TAG );
-        fragMan.commit();
+		FragmentTransaction fragMan = getFragmentManager().beginTransaction();
+		fragMan.add(R.id.mapFrame, mMapFrag_, FRAG2_TAG);
+		fragMan.commit();
 
-        mMap_ = mMapFrag_.getMap(); // Custom MapFragment
-        setUpMapIfNeeded(); // Dynamic
+		mMap_ = mMapFrag_.getMap(); // Custom MapFragment
+		setUpMapIfNeeded(); // Dynamic
 
-    }
+	}
 
-    private void setUpMapIfNeeded()
-    {
-        if ( null != mMap_ )
-        {
-            centerOnCurrentLocation();
-        }
-        else
-        {
-            mMap_ = mMapFrag_.getMap();
-            centerOnCurrentLocation();
-        }
-    }
+	private void setUpMapIfNeeded() {
+		if (null != mMap_) {
+			centerOnCurrentLocation();
+		} else {
+			mMap_ = mMapFrag_.getMap();
+			centerOnCurrentLocation();
+		}
+	}
 
-    /**
-     * Try's to get the GPS location
-     */
-    private void getGPSLocation()
-    {
-        gps_ = new GPSLocator( getApplicationContext() );
+	/**
+	 * Try's to get the GPS location
+	 */
+	private void getGPSLocation() {
+		gps_ = new GPSLocator(getApplicationContext());
 
-        // check if GPS enabled
-        if ( gps_.canGetLocation() )
-        {
-            latitude_ = gps_.getLatitude();
-            longitude_ = gps_.getLongitude();
+		// check if GPS enabled
+		if (gps_.canGetLocation()) {
+			latitude_ = gps_.getLatitude();
+			longitude_ = gps_.getLongitude();
 
-            // Raises a toast to show the location
-            Toast.makeText(
-                    getApplicationContext(),
-                    "Your Location is - \nLat: " + latitude_ + "\nLong: "
-                            + longitude_, Toast.LENGTH_LONG ).show();
-        }
-        else
-        {
-            // There was an error getting the gps information
-            gps_.showSettingsAlert();
-        }
-    }
+			// Raises a toast to show the location
+			Toast.makeText(
+					getApplicationContext(),
+					"Your Location is - \nLat: " + latitude_ + "\nLong: "
+							+ longitude_, Toast.LENGTH_LONG).show();
+		} else {
+			// There was an error getting the gps information
+			gps_.showSettingsAlert();
+		}
+	}
 
-    /**
-     * displays the objectives component to the game screen
-     */
-    private void displayObjectives()
-    {
-        // TODO: change this to actually do what it should do
-        itemAdapter.add( "1.......200" );
-    }
+	/**
+	 * displays the objectives component to the game screen
+	 */
+	private void displayObjectives() {
+		// TODO: change this to actually do what it should do
+		itemAdapter.add("1.......200");
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu( Menu menu )
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate( R.menu.main, menu );
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    public long getQuestStartTime()
-    {
-        return questStartTime_;
-    }
+	public long getQuestStartTime() {
+		return questStartTime_;
+	}
 
-    // ---- Following Methods handle camera implementation -----//
+	// ---- Following Methods handle camera implementation -----//
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see android.app.Activity#onResume()
-     */
-    @Override
-    protected void onResume()
-    {
-        super.onResume();
-        setUpMapIfNeeded();
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onResume()
+	 */
+	@Override
+	protected void onResume() {
+		super.onResume();
+		setUpMapIfNeeded();
+	}
 
-    /**
-     * Focuses the google map on the current GPS location
-     */
-    private void centerOnCurrentLocation()
-    {
-        if ( null != mMap_ )
-        {
-            // Get the location first
-            getGPSLocation();
+	/**
+	 * Focuses the google map on the current GPS location
+	 */
+	private void centerOnCurrentLocation() {
+		if (null != mMap_) {
+			// Get the location first
+			getGPSLocation();
 
-            // Initialize the camera position
-            MapsInitializer.initialize( GameActivity.this );
-            CameraPosition mCameraPosition =
-                    new CameraPosition.Builder()
-                            .zoom( 16.0f )
-                            .target( gps_.getLatLongVariable() ).bearing( 0 )
-                            .tilt( 0 ).build();
+			// Initialize the camera position
+			MapsInitializer.initialize(GameActivity.this);
+			CameraPosition mCameraPosition = new CameraPosition.Builder()
+					.zoom(16.0f).target(gps_.getLatLongVariable()).bearing(0)
+					.tilt(0).build();
 
-            Marker currentPosition =
-                    mMap_.addMarker( new MarkerOptions().position(
-                            gps_.getLatLongVariable() )
-                            .title( "Hamburg" ) );
+			Marker currentPosition = mMap_.addMarker(new MarkerOptions()
+					.position(gps_.getLatLongVariable()).title("Hamburg"));
 
-            Marker newAlienShip =
-                    mMap_.addMarker( new MarkerOptions()
-                            .position( gps_.getLatLongVariable() )
-                            .title( "AlienShip" )
-                            // TODO specify which ship
-                            .snippet( "Ship is landing!" )
-                            .icon( BitmapDescriptorFactory
-                                    .fromResource( R.drawable.alien_ship_map_marker_small ) ) );
+			Marker newAlienShip = mMap_
+					.addMarker(new MarkerOptions()
+							.position(gps_.getLatLongVariable())
+							.title("AlienShip")
+							// TODO specify which ship
+							.snippet("Ship is landing!")
+							.icon(BitmapDescriptorFactory
+									.fromResource(R.drawable.alien_ship_map_marker_small)));
 
-            Toast.makeText(
-                    getApplicationContext(),
-                    "The Aliens are coming!", Toast.LENGTH_LONG ).show();
+			Toast.makeText(getApplicationContext(), "The Aliens are coming!",
+					Toast.LENGTH_LONG).show();
 
-            // Move the camera instantly to hamburg with a zoom of 15.
-            mMap_.moveCamera( CameraUpdateFactory.newLatLngZoom(
-                    gps_.getLatLongVariable(), 15.0f ) );
-            // Zoom in, animating the camera.
-            mMap_.animateCamera( CameraUpdateFactory
-                    .newCameraPosition( mCameraPosition )
-                    );
-        }
-    }
+			// Move the camera instantly to hamburg with a zoom of 15.
+			mMap_.moveCamera(CameraUpdateFactory.newLatLngZoom(
+					gps_.getLatLongVariable(), 15.0f));
+			// Zoom in, animating the camera.
+			mMap_.animateCamera(CameraUpdateFactory
+					.newCameraPosition(mCameraPosition));
+		}
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see android.app.Activity#onPause()
-     */
-    @Override
-    protected void onPause()
-    {
-        super.onPause();
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onPause()
+	 */
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
 
-    /**
-     * This method will display the bulk of the screen as the camera
-     */
-    private void displayCamera()
-    {
-        mTextureView = new TextureView( this );
-        mTextureView.setSurfaceTextureListener( this );
-        RelativeLayout.LayoutParams params;
-        params = new RelativeLayout.LayoutParams( screenWidthPixels_,
-                screenHeightPixels_ );
-        mRelativeLayout = new RelativeLayout( this );
-        mRelativeLayout.addView( mTextureView, params );
-        setContentView( mRelativeLayout );
-    }
+	/**
+	 * This method will display the bulk of the screen as the camera
+	 */
+	private void displayCamera() {
+		mTextureView = new TextureView(this);
+		mTextureView.setSurfaceTextureListener(this);
+		RelativeLayout.LayoutParams params;
+		params = new RelativeLayout.LayoutParams(screenWidthPixels_,
+				screenHeightPixels_);
+		mRelativeLayout = new RelativeLayout(this);
+		mRelativeLayout.addView(mTextureView, params);
+		setContentView(mRelativeLayout);
+	}
 
-    @Override
-    public void onSurfaceTextureAvailable( SurfaceTexture surface, int width,
-            int height )
-    {
-        mCamera = Camera.open();
-        try
-        {
-            mCamera.setPreviewTexture( surface );
-            mCamera.startPreview();
+	@Override
+	public void onSurfaceTextureAvailable(SurfaceTexture surface, int width,
+			int height) {
+		mCamera = Camera.open();
+		try {
+			mCamera.setPreviewTexture(surface);
+			mCamera.startPreview();
 
-        }
-        catch ( IOException ex )
-        {
-            ex.printStackTrace();
-        }
-    }
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
 
-    @Override
-    public void onSurfaceTextureSizeChanged( SurfaceTexture surface,
-            int width,
-            int height )
-    {
-        // TODO Auto-generated method stub
-    }
+	@Override
+	public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width,
+			int height) {
+		// TODO Auto-generated method stub
+	}
 
-    @Override
-    public boolean onSurfaceTextureDestroyed( SurfaceTexture surface )
-    {
-        if ( null != mCamera )
-        {
-            mCamera.stopPreview();
-            mCamera.release();
-        }
-        return true;
-    }
+	@Override
+	public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+		if (null != mCamera) {
+			mCamera.stopPreview();
+			mCamera.release();
+		}
+		return true;
+	}
 
-    @Override
-    public void onSurfaceTextureUpdated( SurfaceTexture surface )
-    {
-        // TODO Auto-generated method stub
-    }
-    // --------------------End of Section-----------------------//
+	@Override
+	public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+		// TODO Auto-generated method stub
+	}
+	// --------------------End of Section-----------------------//
 
 }
--- src/com/example/mapping/CampaignSetUp.java
+++ src/com/example/mapping/CampaignSetUp.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 import com.example.alienquest.GameActivity;
 import com.example.alienquest.R;
@@ -14,139 +15,261 @@ import com.example.alienquest.R;
  * @version Apr 2, 2014
  * 
  */
-public class CampaignSetUp extends GameActivity
-{
-    private static int mNumberOfAlienShips;
+public class CampaignSetUp extends GameActivity {
+	private static int mNumberOfAlienShips;
 
-    /**
-     * There are currently four difficulty options
-     * 
-     * 0 = easyDifficulty
-     * 
-     * 1 = mediumDifficulty (Default)
-     * 
-     * 2 = hardDifficulty
-     * 
-     * 3 = extremeDifficulty
-     */
-    private static int gameDifficulty = 1;
+	/**
+	 * There are currently four difficulty options
+	 * 
+	 * 0 = easyDifficulty
+	 * 
+	 * 1 = mediumDifficulty (Default)
+	 * 
+	 * 2 = hardDifficulty
+	 * 
+	 * 3 = extremeDifficulty
+	 */
+	private static int gameDifficulty = 1;
 
-    /**
-     * There are currently four campaign length options
-     * 
-     * 0 = speedRound (5 minutes)
-     * 
-     * 1 = shortLength (10 minutes)
-     * 
-     * 2 = mediumLength (15 minutes) (Default)
-     * 
-     * 3 = longLength (30 minutes)
-     */
-    private static int gameLengthInMinutes_ = 2;
+	/**
+	 * There are currently four campaign length options
+	 * 
+	 * 0 = speedRound (5 minutes)
+	 * 
+	 * 1 = shortLength (10 minutes)
+	 * 
+	 * 2 = mediumLength (15 minutes) (Default)
+	 * 
+	 * 3 = longLength (30 minutes)
+	 */
+	private static int gameLengthInMinutes_ = 2;
 
-    private static Random randomNumberGenerator_;
+	private static Random randomNumberGenerator_;
 
-    /**
-     * @return NumberOfAlienShips
-     */
-    public int getmNumberOfAlienShips()
-    {
-        return mNumberOfAlienShips;
-    }
+	/**
+	 * @return NumberOfAlienShips
+	 */
+	public int getmNumberOfAlienShips() {
+		return mNumberOfAlienShips;
+	}
 
-    /**
-     * @param mNumberOfAlienShips
-     */
-    public void setmNumberOfAlienShips( int mNumberOfAlienShips )
-    {
-        this.mNumberOfAlienShips = mNumberOfAlienShips;
-    }
+	/**
+	 * @param mNumberOfAlienShips
+	 */
+	public void setmNumberOfAlienShips(int mNumberOfAlienShips) {
+		this.mNumberOfAlienShips = mNumberOfAlienShips;
+	}
 
-    /**
-     * Look at the preference setting and store the values locally.
-     */
-    private CampaignSetUp()
-    {
+	/**
+	 * Look at the preference setting and store the values locally.
+	 */
+	public CampaignSetUp(SharedPreferences initPreferences) {
+		initializePreferences(initPreferences);
+		initializeTheNumberOfAliens();
+	}
 
-    }
+	private void initializePreferences(SharedPreferences preferences) {
+		// Set the difficulty variable
+		String currentDifficultySetting = preferences.getString(
+				getString(R.string.pref_title_difficulty),
+				getString(R.string.pref_title_difficulty));
 
-    private void intializePreferences( SharedPreferences prefernces )
-    {
-        // Set the difficulty variable
-        String currentDifficultySetting = prefernces.getString(
-                getString( R.string.pref_title_difficulty ),
-                getString( R.string.pref_title_difficulty ) );
+		if (currentDifficultySetting
+				.contentEquals(getString(R.string.pref_title_difficulty))
+				|| currentDifficultySetting
+						.contentEquals(getString(R.string.mediumDifficulty))) {
+			// The preference hasnn't been set yet, so default it to medium
+			gameDifficulty = 1;
+		} else if (currentDifficultySetting
+				.contentEquals(getString(R.string.easyDifficulty))) {
+			// Easy
+			gameDifficulty = 0;
+		} else if (currentDifficultySetting
+				.contentEquals(getString(R.string.hardDifficulty))) {
+			// Hard
+			gameDifficulty = 2;
+		} else if (currentDifficultySetting
+				.contentEquals(getString(R.string.extremeDifficulty))) {
+			// Extreme
+			gameDifficulty = 3;
+		}
 
-        if ( currentDifficultySetting
-                .contentEquals( getString( R.string.pref_title_difficulty ) )
-                || currentDifficultySetting
-                        .contentEquals( getString( R.string.mediumDifficulty ) ) )
-        {
-            // The preference hasnn't been set yet, so default it to medium
-            gameDifficulty = 1;
-        }
-        else if ( currentDifficultySetting
-                .contentEquals( getString( R.string.easyDifficulty ) ) )
-        {
-            // Easy
-            gameDifficulty = 0;
-        }
-        else if ( currentDifficultySetting
-                .contentEquals( getString( R.string.hardDifficulty ) ) )
-        {
-            // Hard
-            gameDifficulty = 2;
-        }
-        else if ( currentDifficultySetting
-                .contentEquals( getString( R.string.extremeDifficulty ) ) )
-        {
-            // Extreme
-            gameDifficulty = 3;
-        }
+		// Set the difficulty variable
+		String currentGameLengthSetting = preferences.getString(
+				getString(R.string.pref_title_game_length),
+				getString(R.string.pref_title_game_length));
 
-        
-        
-        // Set the difficulty variable 
-        String currentGameLengthSetting = prefernces.getString(
-                getString( R.string.pref_title_game_length ),
-                getString( R.string.pref_title_game_length ) );
+		if (currentGameLengthSetting
+				.contentEquals(getString(R.string.pref_title_game_length))
+				|| currentDifficultySetting
+						.contentEquals(getString(R.string.mediumLength))) {
+			// The preference hasnn't been set yet, so default it to medium
+			gameLengthInMinutes_ = 1;
+		} else if (currentGameLengthSetting
+				.contentEquals(getString(R.string.speedRound))) {
+			// Easy
+			gameLengthInMinutes_ = 0;
+		} else if (currentGameLengthSetting
+				.contentEquals(getString(R.string.shortLength))) {
+			// Hard
+			gameLengthInMinutes_ = 2;
+		} else if (currentGameLengthSetting
+				.contentEquals(getString(R.string.longLength))) {
+			// Extreme
+			gameLengthInMinutes_ = 3;
+		}
+	}
 
-        if ( currentGameLengthSetting
-                .contentEquals( getString( R.string.pref_title_game_length ) )
-                || currentDifficultySetting
-                        .contentEquals( getString( R.string.mediumLength ) ) )
-        {
-            // The preference hasnn't been set yet, so default it to medium
-            gameLengthInMinutes_ = 1;
-        }
-        else if ( currentGameLengthSetting
-                .contentEquals( getString( R.string.speedRound ) ) )
-        {
-            // Easy
-            gameLengthInMinutes_ = 0;
-        }
-        else if ( currentGameLengthSetting
-                .contentEquals( getString( R.string.shortLength ) ) )
-        {
-            // Hard
-            gameLengthInMinutes_ = 2;
-        }
-        else if ( currentGameLengthSetting
-                .contentEquals( getString( R.string.longLength ) ) )
-        {
-            // Extreme
-            gameLengthInMinutes_ = 3;
-        }
-    }
+	/**
+	 * There are two main things taken into account here. The first is the time
+	 * of the campaign and the second is the selected difficulty.
+	 * 
+	 * @return the number of aliens created
+	 */
+	private int initializeTheNumberOfAliens() {
+		randomNumberGenerator_ = new Random();
+		int randomInt = 0;
 
-    /**
-     * There are two main things taken into account here. The first is the time
-     * of the campaign and the second is the selected difficulty.
-     * 
-     * @return the number of aliens created
-     */
-    private int initializeTheNumberOfAliens()
-    {
-        return -1;
-    }
+		switch (gameDifficulty) {
+		case 0:
+
+			/**
+			 * EASY
+			 * 
+			 * 5 minutes: 5 - 14
+			 * 
+			 * 10 minutes: 10 - 19
+			 * 
+			 * 15 minutes: 15 - 24
+			 * 
+			 * 30 minutes: 30 - 39
+			 * 
+			 */
+
+			if (gameLengthInMinutes_ == 0) {
+
+				randomInt = randomNumberGenerator_.nextInt(10) + 5;
+
+			} else if (gameLengthInMinutes_ == 1) {
+
+				randomInt = randomNumberGenerator_.nextInt(10) + 10;
+
+			} else if (gameLengthInMinutes_ == 2) {
+
+				randomInt = randomNumberGenerator_.nextInt(10) + 15;
+
+			} else {
+
+				randomInt = randomNumberGenerator_.nextInt(10) + 30;
+
+			}
+			Log.i("Random int is ", "" + randomInt);
+			break;
+		case 1:
+			/**
+			 * MEDIUM
+			 * 
+			 * 5 minutes: 15 - 24
+			 * 
+			 * 10 minutes: 30 - 39
+			 * 
+			 * 15 minutes: 45 - 54
+			 * 
+			 * 30 minutes: 90 - 99
+			 * 
+			 */
+
+			if (gameLengthInMinutes_ == 0) {
+
+				randomInt = randomNumberGenerator_.nextInt(10) + 15;
+
+			} else if (gameLengthInMinutes_ == 1) {
+
+				randomInt = randomNumberGenerator_.nextInt(10) + 30;
+
+			} else if (gameLengthInMinutes_ == 2) {
+
+				randomInt = randomNumberGenerator_.nextInt(10) + 45;
+
+			} else {
+
+				randomInt = randomNumberGenerator_.nextInt(10) + 90;
+
+			}
+			Log.i("Random int is ", "" + randomInt);
+			break;
+		case 2:
+
+			/**
+			 * HARD
+			 * 
+			 * 5 minutes: 25 - 34
+			 * 
+			 * 10 minutes: 50 - 59
+			 * 
+			 * 15 minutes: 75 - 84
+			 * 
+			 * 30 minutes: 150 - 159
+			 * 
+			 */
+
+			if (gameLengthInMinutes_ == 0) {
+
+				randomInt = randomNumberGenerator_.nextInt(10) + 25;
+
+			} else if (gameLengthInMinutes_ == 1) {
+
+				randomInt = randomNumberGenerator_.nextInt(10) + 50;
+
+			} else if (gameLengthInMinutes_ == 2) {
+
+				randomInt = randomNumberGenerator_.nextInt(10) + 75;
+
+			} else {
+
+				randomInt = randomNumberGenerator_.nextInt(10) + 150;
+
+			}
+			Log.i("Random int is ", "" + randomInt);
+
+			break;
+		case 3:
+			/**
+			 * EXTREME
+			 * 
+			 * 5 minutes: 30 - 39
+			 * 
+			 * 10 minutes: 60 - 69
+			 * 
+			 * 15 minutes: 90 - 99
+			 * 
+			 * 30 minutes: 180 - 189
+			 * 
+			 */
+
+			if (gameLengthInMinutes_ == 0) {
+
+				randomInt = randomNumberGenerator_.nextInt(10) + 30;
+
+			} else if (gameLengthInMinutes_ == 1) {
+
+				randomInt = randomNumberGenerator_.nextInt(10) + 60;
+
+			} else if (gameLengthInMinutes_ == 2) {
+
+				randomInt = randomNumberGenerator_.nextInt(10) + 90;
+
+			} else {
+
+				randomInt = randomNumberGenerator_.nextInt(10) + 180;
+
+			}
+			Log.i("Random int is ", "" + randomInt);
+			break;
+		default:
+			break;
+		}
+		setmNumberOfAlienShips(randomInt);
+		return getmNumberOfAlienShips();
+	}
 }

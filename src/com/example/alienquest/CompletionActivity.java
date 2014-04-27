a0ddfcd5ab2d4451918b9b81f4ca7fa3e23dcc4d
Tiffany <tiffany.liana@gmail.com>
2014-04-27 17:04:17 -0400

    Updated CompletionActivity

--- src/com/example/alienquest/CompletionActivity.java
+++ src/com/example/alienquest/CompletionActivity.java
@@ -1,8 +1,15 @@
 package com.example.alienquest;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * @author Jimmy Dagres
@@ -12,11 +19,28 @@ import android.view.Menu;
  */
 public class CompletionActivity extends Activity {
 
+	private Intent settings;
+	protected static SharedPreferences preference_;
+	private String userName_;
+	private TextView congratulationsName_;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_completion);
-		
+
+		settings = getIntent();
+		// Load the preference values
+		// Set up the preference
+		preference_ = getSharedPreferences(getString(R.string.pref_title_file),
+				Context.MODE_PRIVATE);
+		userName_ = preference_.getString(getString(R.string.pref_title_name),
+				getString(R.string.pref_title_name));
+
+		// Format string to include username
+		congratulationsName_ = (TextView) findViewById(R.id.congratulations_name);
+		String nameMsg = getString(R.string.congratulations_name, userName_);
+		congratulationsName_.setText(nameMsg);
 	}
 
 	@Override
@@ -26,7 +50,17 @@ public class CompletionActivity extends Activity {
 		return true;
 	}
 
-	private void orderHighScore() {
-
+	private void getScores() {
+		// // Setting preferences
+		// SharedPreferences prefs = this.getSharedPreferences("myPrefsKey",
+		// Context.MODE_PRIVATE);
+		// Editor editor = prefs.edit();
+		// editor.putInt("key", score);
+		// editor.commit();
+		//
+		// // getting preferences
+		// SharedPreferences prefs = this.getSharedPreferences("myPrefsKey",
+		// Context.MODE_PRIVATE);
+		// int score = prefs.getInt("key", 0); // 0 is the default value
 	}
 }

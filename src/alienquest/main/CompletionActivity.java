6bcbef6668fe50e356f726826405eb942ec02aa0
Tiffany <tiffany.liana@gmail.com>
2014-05-02 21:34:25 -0400

    Added Highscores button and fixed a spelling error in CompletionActivity

--- src/alienquest/main/CompletionActivity.java
+++ src/alienquest/main/CompletionActivity.java
@@ -18,216 +18,194 @@ import android.widget.Toast;
  * @version Apr 1, 2014
  * 
  * 
- *          I (Jimmy) chose to store the highscores in a string stored as
- *          a preference, rather than a database because I wanted to practice with
+ *          I (Jimmy) chose to store the highscores in a string stored as a
+ *          preference, rather than a database because I wanted to practice with
  *          the android favored data structure SparseArray's.
  */
-public class CompletionActivity extends Activity
-{
+public class CompletionActivity extends Activity {
 
-    private static final String PREF_NAME = "myScores";
-    protected static SharedPreferences preference_;
-    private Intent settings;
-    private String userName_;
-    private TextView congratulationsName_;
+	private static final String PREF_NAME = "myScores";
+	protected static SharedPreferences preference_;
+	private Intent settings;
+	private String userName_;
+	private TextView congratulationsName_;
 
-    private String topFiveScoreValues_;
-    private String topFiveScoreUsernames_;
+	private String topFiveScoreValues_;
+	private String topFiveScoreUsernames_;
 
-    // Array map that contains the top 5 scores as keys, and the associated
-    // usernames as values
-    SparseArray<String> highscores_;
+	// Array map that contains the top 5 scores as keys, and the associated
+	// usernames as values
+	SparseArray<String> highscores_;
 
-    /**
-     * Delim used to separate the highscores in the preference string
-     */
-    static String delim = "/b";
+	/**
+	 * Delim used to separate the highscores in the preference string
+	 */
+	static String delim = "/b";
 
-    @Override
-    protected void onCreate( Bundle savedInstanceState )
-    {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_completion );
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_completion);
 
-        // TODO: get the values sent in the bundle.
+		// TODO: get the values sent in the bundle.
 
-        settings = getIntent();
+		settings = getIntent();
 
-        highscores_ = new SparseArray<String>();
+		highscores_ = new SparseArray<String>();
 
-        int score = getScores(); // TODO: implement database of scores
-        // Load the preference values
-        // Set up the preference
-        preference_ =
-                getSharedPreferences( getString( R.string.pref_title_file ),
-                        Context.MODE_PRIVATE );
-        userName_ =
-                preference_.getString( getString( R.string.pref_title_name ),
-                        getString( R.string.pref_title_name ) );
+		int score = getScores(); // TODO: implement database of scores
+		// Load the preference values
+		// Set up the preference
+		preference_ = getSharedPreferences(getString(R.string.pref_title_file),
+				Context.MODE_PRIVATE);
+		userName_ = preference_.getString(getString(R.string.pref_title_name),
+				getString(R.string.pref_title_name));
 
-        // Get the bundle
-        Bundle b = settings.getExtras();
-        if ( b != null )
-        {
-            int scoreTotal = Integer.parseInt( b.get( getString( R.string.key_score ) ).toString() );
-            boolean gameComplete =
-                    Boolean.parseBoolean( b.get( getString( R.string.key_completed ) ).toString() );
-            boolean callCompletedMethod =
-                    Boolean.parseBoolean( b.get( getString( R.string.key_display_completed) ).toString() );
+		// Get the bundle
+		Bundle b = settings.getExtras();
+		if (b != null) {
+			int scoreTotal = Integer.parseInt(b.get(
+					getString(R.string.key_score)).toString());
+			boolean gameComplete = Boolean.parseBoolean(b.get(
+					getString(R.string.key_completed)).toString());
+			boolean callCompletedMethod = Boolean.parseBoolean(b.get(
+					getString(R.string.key_display_completed)).toString());
 
-            if ( callCompletedMethod )
-            {
-                gameEnded( scoreTotal, gameComplete );
-            }
+			if (callCompletedMethod) {
+				gameEnded(scoreTotal, gameComplete);
+			}
 
-        }
+		}
 
-        // Format string to include username TODO: Jimmy: I'm not sure what this
-        // is?
-        // congratulationsName_ =
-        // (TextView) findViewById( R.id.congratulations_name );
-        // String nameMsg = getString( R.string.congratulations_name, userName_
-        // );
-        // congratulationsName_.setText( nameMsg );
+		// Format string to include username TODO: Jimmy: I'm not sure what this
+		// is?
+		// congratulationsName_ =
+		// (TextView) findViewById( R.id.congratulations_name );
+		// String nameMsg = getString( R.string.congratulations_name, userName_
+		// );
+		// congratulationsName_.setText( nameMsg );
 
-        getTopFiveScores();
-    }
+		getTopFiveScores();
+	}
 
-    /**
-     * @param score
-     * @param completed
-     *            whether or not the game was successful beat
-     */
-    public void gameEnded( int score, boolean completed )
-    {
-        if ( completed )
-        {
-            congratulationsName_ =
-                    (TextView) findViewById( R.id.congratulations_name );
-            // String nameMsg =
-            // getString( R.string.congratulations_name, userName_ );
-            String nameMsg =
-                    "Congradulations "
-                            + userName_
-                            + " you completed the quest! Earth thanks you. \n Your score was "
-                            + score + ".";
-            congratulationsName_.setText( nameMsg );
+	/**
+	 * @param score
+	 * @param completed
+	 *            whether or not the game was successful beat
+	 */
+	public void gameEnded(int score, boolean completed) {
+		if (completed) {
+			congratulationsName_ = (TextView) findViewById(R.id.congratulations_name);
+			// String nameMsg =
+			// getString( R.string.congratulations_name, userName_ );
+			String nameMsg = "Congratulations "
+					+ userName_
+					+ " you completed the quest! Earth thanks you. \n Your score was "
+					+ score + ".";
+			congratulationsName_.setText(nameMsg);
 
-            // See if the new score is higher than the 5th best score, if so
-            // place it into the sparse array and remove the fifth value, then
-            // call a function to update the preference settings
-            if ( score > highscores_.keyAt( 4 ) )
-            {
-                highscores_.remove( highscores_.keyAt( 4 ) );
-                highscores_.put( score, userName_ );
-                updatedSavedHighSchores();
+			// See if the new score is higher than the 5th best score, if so
+			// place it into the sparse array and remove the fifth value, then
+			// call a function to update the preference settings
+			if (score > highscores_.keyAt(4)) {
+				highscores_.remove(highscores_.keyAt(4));
+				highscores_.put(score, userName_);
+				updatedSavedHighSchores();
 
-                // Update the edit texts with the new top 5 scores
-                displayTopFiveScores();
+				// Update the edit texts with the new top 5 scores
+				displayTopFiveScores();
 
-                Toast.makeText(
-                        getApplicationContext(),
-                        "Congradulations " + userName_ + "! Your score of "
-                                + score
-                                + " is a top five number!", Toast.LENGTH_SHORT )
-                        .show();
-            }
-        }
-        else
-        {
-            // Tell the user that time ran out
-            congratulationsName_.setText( userName_ );
-        }
-    }
+				Toast.makeText(
+						getApplicationContext(),
+						"Congradulations " + userName_ + "! Your score of "
+								+ score + " is a top five number!",
+						Toast.LENGTH_SHORT).show();
+			}
+		} else {
+			// Tell the user that time ran out
+			congratulationsName_.setText(userName_);
+		}
+	}
 
-    /**
-     * This method will put the sparseArray into a string with the proper delim
-     * format and will save it as a preference
-     */
-    private void updatedSavedHighSchores()
-    {
-        // TODO Auto-generated method stub
+	/**
+	 * This method will put the sparseArray into a string with the proper delim
+	 * format and will save it as a preference
+	 */
+	private void updatedSavedHighSchores() {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu( Menu menu )
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate( R.menu.main, menu );
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    /**
-     * This method saves the scores to the string in the preferences
-     */
-    private void saveScores()
-    {
-        // setting preferences
-        // SharedPreferences prefs = this.getSharedPreferences("myPrefsKey",
-        // Context.MODE_PRIVATE);
-        // Editor editor = prefs.edit();
-        // editor.putInt("key", score);
-        // editor.commit();
+	/**
+	 * This method saves the scores to the string in the preferences
+	 */
+	private void saveScores() {
+		// setting preferences
+		// SharedPreferences prefs = this.getSharedPreferences("myPrefsKey",
+		// Context.MODE_PRIVATE);
+		// Editor editor = prefs.edit();
+		// editor.putInt("key", score);
+		// editor.commit();
 
-    }
+	}
 
-    /**
-     * Parses through the top 5 names and scores and adds them to the map
-     */
-    private void getTopFiveScores()
-    {
+	/**
+	 * Parses through the top 5 names and scores and adds them to the map
+	 */
+	private void getTopFiveScores() {
 
-        topFiveScoreValues_ = preference_.getString(
-                getString( R.string.pref_title_top_five_scores ),
-                getString( R.string.pref_title_top_five_scores ) );
+		topFiveScoreValues_ = preference_.getString(
+				getString(R.string.pref_title_top_five_scores),
+				getString(R.string.pref_title_top_five_scores));
 
-        topFiveScoreUsernames_ = preference_.getString(
-                getString( R.string.pref_title_top_five_names ),
-                getString( R.string.pref_title_top_five_names ) );
+		topFiveScoreUsernames_ = preference_.getString(
+				getString(R.string.pref_title_top_five_names),
+				getString(R.string.pref_title_top_five_names));
 
-        String[] arrayOfScores = topFiveScoreValues_.split( delim );
-        String[] arrayOfNames = topFiveScoreUsernames_.split( delim );
+		String[] arrayOfScores = topFiveScoreValues_.split(delim);
+		String[] arrayOfNames = topFiveScoreUsernames_.split(delim);
 
-        int j = 0;
-        for ( ; arrayOfNames.length < j && arrayOfScores.length < j; j++ )
-        {
-            highscores_.put( Integer.parseInt( arrayOfScores[j] ),
-                    arrayOfNames[j] );
-        }
+		int j = 0;
+		for (; arrayOfNames.length < j && arrayOfScores.length < j; j++) {
+			highscores_
+					.put(Integer.parseInt(arrayOfScores[j]), arrayOfNames[j]);
+		}
 
-        // If there isn't five scores saved then make them blank
-        for ( ; j < 5; j++ )
-        {
-            highscores_.put( 0, "Empty" );
-        }
+		// If there isn't five scores saved then make them blank
+		for (; j < 5; j++) {
+			highscores_.put(0, "Empty");
+		}
 
-        // Call a function method to update the edit texts with the top 5 scores
-        displayTopFiveScores();
-    }
+		// Call a function method to update the edit texts with the top 5 scores
+		displayTopFiveScores();
+	}
 
-    /**
-     * Update the edit texts with the top 5 scores
-     */
-    private void displayTopFiveScores()
-    {
-        // TODO Auto-generated method stub
-    }
+	/**
+	 * Update the edit texts with the top 5 scores
+	 */
+	private void displayTopFiveScores() {
+		// TODO Auto-generated method stub
+	}
 
-    /**
-     * Updates the score in the saved string.
-     * 
-     * @param newScore
-     * @param userName
-     */
-    private void updateScore( int newScore, String userName )
-    {
+	/**
+	 * Updates the score in the saved string.
+	 * 
+	 * @param newScore
+	 * @param userName
+	 */
+	private void updateScore(int newScore, String userName) {
 
-    }
+	}
 
-    private int getScores()
-    {
-        // getting preferences
-        return settings.getIntExtra( "score", 0 ); // 0 is the default value
-    }
+	private int getScores() {
+		// getting preferences
+		return settings.getIntExtra("score", 0); // 0 is the default value
+	}
 }
--- src/alienquest/main/MainActivity.java
+++ src/alienquest/main/MainActivity.java
@@ -29,255 +29,240 @@ import android.widget.TextView;
  * @version Mar 30, 2014
  * 
  */
-public class MainActivity extends Activity
-{
-    /**
-     * Used for the settings
-     */
-    public static boolean inputCorrect;
+public class MainActivity extends Activity {
+	/**
+	 * Used for the settings
+	 */
+	public static boolean inputCorrect;
 
-    /**
-     * determines if the app is in its initial setup
-     */
-    private static boolean initialSetup;
+	/**
+	 * determines if the app is in its initial setup
+	 */
+	private static boolean initialSetup;
 
-    /**
-     * settings menu Intent, used to access settings and preference information
-     */
-    private Intent settingsIntention;
+	/**
+	 * settings menu Intent, used to access settings and preference information
+	 */
+	private Intent settingsIntention;
 
-    /**
-     * start game intent
-     */
-    private Intent startGameIntention;
+	/**
+	 * start game intent
+	 */
+	private Intent startGameIntention;
 
-    private Intent startTestIntention;
-    /**
-     * instance of the settings task
-     */
-    private static SharedPreferences preference_;
+	private Intent startScoreIntention;
+	/**
+	 * instance of the settings task
+	 */
+	private static SharedPreferences preference_;
 
-    // This variable is updated from the settings and determines if there will
-    // be sound in the game
-    private boolean soundEnabled = false;
+	// This variable is updated from the settings and determines if there will
+	// be sound in the game
+	private boolean soundEnabled = false;
 
-    // Button instances
-    private Button startGameButton_;
-    private Button instructionsButton_;
-    private Button aboutGameButton_;
-    private Button testButton_;
+	// Button instances
+	private Button startGameButton_;
+	private Button instructionsButton_;
+	private Button aboutGameButton_;
+	private Button scoresButton_;
 
-    private TextView welcomeMsg_;
-    private TextView welcomeMsg2_;
+	private TextView welcomeMsg_;
+	private TextView welcomeMsg2_;
 
-    @Override
-    protected void onCreate( Bundle savedInstanceState )
-    {
-        super.onCreate( savedInstanceState );
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        // Opens alert dialog if user GPS is not enabled
-        LocationManager lm =
-                (LocationManager) getSystemService( LOCATION_SERVICE );
-        if ( !lm.isProviderEnabled( LocationManager.GPS_PROVIDER )
-                || !lm.isProviderEnabled( LocationManager.NETWORK_PROVIDER ) )
-        {
+		// Opens alert dialog if user GPS is not enabled
+		LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+		if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)
+				|| !lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
 
-            // Build the alert dialog
-            AlertDialog.Builder builder = new AlertDialog.Builder( this );
-            builder.setTitle( "Location Services Not Active" );
-            builder.setMessage( "Please enable Location Services and GPS" );
-            builder.setPositiveButton( "OK",
-                    new DialogInterface.OnClickListener()
-                    {
-                        public void onClick( DialogInterface dialogInterface,
-                                int i )
-                        {
-                            // Show location settings when the user acknowledges
-                            // the alert dialog
-                            Intent intent = new Intent(
-                                    Settings.ACTION_LOCATION_SOURCE_SETTINGS );
-                            startActivity( intent );
-                        }
-                    } );
-            Dialog alertDialog = builder.create();
-            alertDialog.setCanceledOnTouchOutside( false );
-            alertDialog.show();
-        }
+			// Build the alert dialog
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle("Location Services Not Active");
+			builder.setMessage("Please enable Location Services and GPS");
+			builder.setPositiveButton("OK",
+					new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialogInterface,
+								int i) {
+							// Show location settings when the user acknowledges
+							// the alert dialog
+							Intent intent = new Intent(
+									Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+							startActivity(intent);
+						}
+					});
+			Dialog alertDialog = builder.create();
+			alertDialog.setCanceledOnTouchOutside(false);
+			alertDialog.show();
+		}
 
-        setContentView( R.layout.activity_main );
+		setContentView(R.layout.activity_main);
 
-        Typeface tf =
-                Typeface.createFromAsset( getAssets(), "Fonts/Molot.otf" );
-        Typeface main = Typeface
-                .createFromAsset( getAssets(), "Fonts/OpTic.ttf" );
+		Typeface tf = Typeface.createFromAsset(getAssets(), "Fonts/Molot.otf");
+		Typeface main = Typeface
+				.createFromAsset(getAssets(), "Fonts/OpTic.ttf");
 
-        settingsIntention = new Intent( MainActivity.this,
-                SettingsPreferenceActivity.class );
-        startGameIntention =
-                new Intent( MainActivity.this, GameActivity.class );
-        startTestIntention = new Intent( MainActivity.this,
-                CompletionActivity.class );
+		settingsIntention = new Intent(MainActivity.this,
+				SettingsPreferenceActivity.class);
+		startGameIntention = new Intent(MainActivity.this, GameActivity.class);
+		startScoreIntention = new Intent(MainActivity.this,
+				CompletionActivity.class);
 
-        // Set up the start game, instructions and about buttons.
-        startGameButton_ = (Button) findViewById( R.id.startGameButton );
-        instructionsButton_ = (Button) findViewById( R.id.instructionsButton );
-        aboutGameButton_ = (Button) findViewById( R.id.aboutButton );
+		// Set up the start game, instructions and about buttons.
+		startGameButton_ = (Button) findViewById(R.id.startGameButton);
+		instructionsButton_ = (Button) findViewById(R.id.instructionsButton);
+		aboutGameButton_ = (Button) findViewById(R.id.aboutButton);
+		scoresButton_ = (Button) findViewById(R.id.scoresButton);
 
-        welcomeMsg_ = (TextView) findViewById( R.id.textView1 );
-        welcomeMsg2_ = (TextView) findViewById( R.id.textView2 );
+		welcomeMsg_ = (TextView) findViewById(R.id.textView1);
+		welcomeMsg2_ = (TextView) findViewById(R.id.textView2);
 
-        startGameButton_.setTypeface( tf );
-        instructionsButton_.setTypeface( tf );
-        aboutGameButton_.setTypeface( tf );
+		startGameButton_.setTypeface(tf);
+		instructionsButton_.setTypeface(tf);
+		aboutGameButton_.setTypeface(tf);
+		scoresButton_.setTypeface(tf);
 
-        welcomeMsg_.setTypeface( tf );
-        welcomeMsg2_.setTypeface( main );
+		welcomeMsg_.setTypeface(tf);
+		welcomeMsg2_.setTypeface(main);
 
-        // Set up the preference
-        preference_ =
-                getSharedPreferences( getString( R.string.pref_title_file ),
-                        Context.MODE_PRIVATE );
+		// Set up the preference
+		preference_ = getSharedPreferences(getString(R.string.pref_title_file),
+				Context.MODE_PRIVATE);
 
-        // Set up sound
-        initializeSound();
+		// Set up sound
+		initializeSound();
 
-        // Start game
-        startGameButton_.setOnClickListener( new View.OnClickListener()
-        {
-            @Override
-            public void onClick( View v )
-            {
-                Vibrator earthShaker =
-                        (Vibrator) getSystemService( Context.VIBRATOR_SERVICE );
-                earthShaker.vibrate( 200 );
-                // startGameIntention.putExtra("Difficulty",
-                // settingsIntention.getStringExtra("Difficulty"));
-                MainActivity.this.startActivity( startGameIntention );
-            }
-        } );
+		// Start game
+		startGameButton_.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Vibrator earthShaker = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+				earthShaker.vibrate(200);
+				// startGameIntention.putExtra("Difficulty",
+				// settingsIntention.getStringExtra("Difficulty"));
+				MainActivity.this.startActivity(startGameIntention);
+			}
+		});
 
-        // Display instructions fragment
-        instructionsButton_.setOnClickListener( new View.OnClickListener()
-        {
-            @Override
-            public void onClick( View v )
-            {
-                displayInstructionsDialog();
-            }
-        } );
+		// Display instructions fragment
+		instructionsButton_.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				displayInstructionsDialog();
+			}
+		});
 
-        // Display about fragment
-        aboutGameButton_.setOnClickListener( new View.OnClickListener()
-        {
-            @Override
-            public void onClick( View v )
-            {
-                displayAboutDialog();
-            }
-        } );
+		// Display about fragment
+		aboutGameButton_.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				displayAboutDialog();
+			}
+		});
 
-        // CUSTOM
-        // Test the map fragment
-        // mapFragment_ = new MapFragmentClass();
-        //
-        // FragmentManager fragmentManager = getFragmentManager();
-        // FragmentTransaction fragmentTransaction =
-        // fragmentManager.beginTransaction();
-        //
-        // fragmentTransaction.add( R.id.googleMapFragment, mapFragment_ );
-        // fragmentTransaction.commit();
+		scoresButton_.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				MainActivity.this.startActivity(startScoreIntention);
+			}
+		});
 
-        // mMap_ = new GoogleMap( null ); TODO
+		// CUSTOM
+		// Test the map fragment
+		// mapFragment_ = new MapFragmentClass();
+		//
+		// FragmentManager fragmentManager = getFragmentManager();
+		// FragmentTransaction fragmentTransaction =
+		// fragmentManager.beginTransaction();
+		//
+		// fragmentTransaction.add( R.id.googleMapFragment, mapFragment_ );
+		// fragmentTransaction.commit();
 
-    }
+		// mMap_ = new GoogleMap( null ); TODO
 
-    /**
-     * Gets the sound preference and initialize the variable 
-     */
-    private void initializeSound()
-    {
-        String soundSring = preference_.getString(
-                getString( R.string.pref_title_sound ),
-                getString( R.string.pref_title_sound ) );
+	}
 
-        soundEnabled = soundSring.toUpperCase().contains( "TRUE" );
-    }
+	/**
+	 * Gets the sound preference and initialize the variable
+	 */
+	private void initializeSound() {
+		String soundSring = preference_.getString(
+				getString(R.string.pref_title_sound),
+				getString(R.string.pref_title_sound));
 
-    /**
-     * Adds the instructions fragment to the view
-     */
-    private void displayInstructionsDialog()
-    {
-        // prepare the instructions box
-        Dialog instructionsBox = new Dialog( MainActivity.this );
-        instructionsBox.setContentView( R.layout.dialog_instructions );
-        instructionsBox.getWindow().setBackgroundDrawable(
-                new ColorDrawable( android.graphics.Color.TRANSPARENT ) );
-        // set the message to display
-        instructionsBox.setTitle( "Instructions Menu" );
+		soundEnabled = soundSring.toUpperCase().contains("TRUE");
+	}
 
-        instructionsBox.show();
-    }
+	/**
+	 * Adds the instructions fragment to the view
+	 */
+	private void displayInstructionsDialog() {
+		// prepare the instructions box
+		Dialog instructionsBox = new Dialog(MainActivity.this);
+		instructionsBox.setContentView(R.layout.dialog_instructions);
+		instructionsBox.getWindow().setBackgroundDrawable(
+				new ColorDrawable(android.graphics.Color.TRANSPARENT));
+		// set the message to display
+		instructionsBox.setTitle("Instructions Menu");
 
-    /**
-     * Adds the about fragment to the view
-     */
-    private void displayAboutDialog()
-    {
-        // prepare the instructions box
-        Dialog aboutBox = new Dialog( MainActivity.this );
-        aboutBox.setContentView( R.layout.dialog_about );
+		instructionsBox.show();
+	}
 
-        // TextView aboutTxt = (TextView) aboutBox
-        // .findViewById(android.R.id.message);
+	/**
+	 * Adds the about fragment to the view
+	 */
+	private void displayAboutDialog() {
+		// prepare the instructions box
+		Dialog aboutBox = new Dialog(MainActivity.this);
+		aboutBox.setContentView(R.layout.dialog_about);
 
-        // set the message to display
-        aboutBox.getWindow().setBackgroundDrawable(
-                new ColorDrawable( android.graphics.Color.TRANSPARENT ) );
-        aboutBox.setTitle( "About Alien Quest" );
-        aboutBox.show();
-    }
+		// TextView aboutTxt = (TextView) aboutBox
+		// .findViewById(android.R.id.message);
 
-    @Override
-    public boolean onOptionsItemSelected( MenuItem item )
-    {
-        switch ( item.getItemId() )
-        {
-        case R.id.action_settings:
-            this.startActivity( settingsIntention );
+		// set the message to display
+		aboutBox.getWindow().setBackgroundDrawable(
+				new ColorDrawable(android.graphics.Color.TRANSPARENT));
+		aboutBox.setTitle("About Alien Quest");
+		aboutBox.show();
+	}
 
-            break;
-        }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			this.startActivity(settingsIntention);
 
-        return true;
-    }
+			break;
+		}
 
-    @Override
-    public boolean onCreateOptionsMenu( Menu menu )
-    {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate( R.menu.settings, menu );
-        return true;
-    }
+		return true;
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see android.app.Activity#onMenuItemSelected(int, android.view.MenuItem)
-     */
-    @Override
-    public boolean onMenuItemSelected( int featureId, MenuItem item )
-    {
-        // TODO Auto-generated method stub
-        return super.onMenuItemSelected( featureId, item );
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.settings, menu);
+		return true;
+	}
 
-    /**
-     * @return
-     * @returns the preferences
-     */
-    public static SharedPreferences getPreference_()
-    {
-        return preference_;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onMenuItemSelected(int, android.view.MenuItem)
+	 */
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		// TODO Auto-generated method stub
+		return super.onMenuItemSelected(featureId, item);
+	}
+
+	/**
+	 * @return
+	 * @returns the preferences
+	 */
+	public static SharedPreferences getPreference_() {
+		return preference_;
+	}
 }
